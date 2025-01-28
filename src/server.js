207977d4efbd3a8d4106734e070df83f31bca44c
Jasper-ops698 <bkitib2@gmail.com>
2025-01-29 02:29:19 +0300

    Update port

--- src/server.js
+++ src/server.js
@@ -91,8 +91,8 @@ connectToDatabase()
 
     // Start the Server
     const PORT = process.env.PORT || 5000;
-    app.listen(PORT, () => {
-      console.log(`Server running on port ${PORT}`);
+    app.listen(PORT, '0.0.0.0', () => {
+      console.log(`Server running on http://0.0.0.0:${PORT}`);
     });
   })
   .catch((err) => {
