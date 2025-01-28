cee6e2510dfcda0bff333d2d9ad9dc4b1f707a1f
Jasper-ops698 <bkitib2@gmail.com>
2025-01-28 13:31:39 +0300

    Update server.js

--- src/server.js
+++ src/server.js
@@ -10,6 +10,8 @@ const connectToDatabase = require("./db");
 
 dotenv.config(); // Load environment variables
 
+const port = process.env.PORT || 5000;
+
 const app = express();
 
 // Middleware: Security and Performance
