dc4dd0dfc40fd3756590beabf637f25e0ad9b9b4
Jasper-ops698 <bkitib2@gmail.com>
2025-01-29 08:24:30 +0300

    Update server.js

--- src/server.js
+++ src/server.js
@@ -28,7 +28,7 @@ app.use(limiter);
 // CORS Configuration
 const allowedOrigins = process.env.ALLOWED_ORIGINS
   ? process.env.ALLOWED_ORIGINS.split(",")
-  : ["http://localhost:3000"; // Default origin
+  : ["http://localhost:3000"]; // Default origin
 
 app.use(
   cors({
