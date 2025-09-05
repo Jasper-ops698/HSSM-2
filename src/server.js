3729c79c9567f19b2c6be69e80ea98adb94e918a
Jasper <bkitib@gmail.com>
2025-09-05 08:00:31 +0300

    O

--- src/server.js
+++ src/server.js
@@ -24,7 +24,10 @@ dotenv.config();
 const app = express();
 // --- Global Middleware ---
 app.use(helmet());
-app.use(cors());
+app.use(cors({
+  origin: 'https://hssm-services.web.app',
+  credentials: true,
+}));
 app.use(morgan('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
