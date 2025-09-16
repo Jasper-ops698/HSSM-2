3260935b21a943900b7219df02d6ff1ca8d2375d
Jasper <bkitib@gmail.com>
2025-09-16 07:55:22 +0300

    NEW

--- routes/absenceRoutes.js
+++ routes/absenceRoutes.js
@@ -1,21 +1,22 @@
 const express = require('express');
 const router = express.Router();
 const absenceController = require('../controllers/absenceController');
-const { protect, teacher, hod } = require('../middlewares/authMiddleware');
+const { protect } = require('../middlewares/authMiddleware');
+const verifyRole = require('../middlewares/verifyRole');
 
 // @route   POST /api/absence/report
 // @desc    Report absence (for teachers)
 // @access  Private/Teacher
-router.post('/report', protect, teacher, absenceController.reportAbsence);
+router.post('/report', protect, verifyRole(['teacher']), absenceController.reportAbsence);
 
 // @route   GET /api/absence
 // @desc    Get absences for HOD's department
 // @access  Private/HOD
-router.get('/', protect, hod, absenceController.getAbsences);
+router.get('/', protect, verifyRole(['HOD']), absenceController.getAbsences);
 
 // @route   POST /api/absence/assign-replacement
 // @desc    Assign replacement teacher
 // @access  Private/HOD
-router.post('/assign-replacement', protect, hod, absenceController.assignReplacement);
+router.post('/assign-replacement', protect, verifyRole(['HOD']), absenceController.assignReplacement);
 
 module.exports = router;
